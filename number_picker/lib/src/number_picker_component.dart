7de166f65bc21b98e0d4b50f2144e15b9105336d
negahdaripour <negahdari.ce.shirazu@gmail.com>
2022-01-08 14:34:37 +0330

    number_picker test completed

--- number_picker/lib/src/number_picker_component.dart
+++ number_picker/lib/src/number_picker_component.dart
@@ -14,7 +14,10 @@ class NumberPicker extends StatelessWidget {
       required final this.maxValue,
       required final this.minValue,
       final Key? key})
-      : super(key: key);
+      : assert(minValue < maxValue, 'minValue has to be smaller than maxValue'),
+        assert(initialValue >= minValue && initialValue <= maxValue,
+            'initialValue has to be between minValue and maxvalue'),
+        super(key: key);
 
   @override
   Widget build(final BuildContext context) => Row(
--- number_picker/test/number_picker_test.dart
+++ number_picker/test/number_picker_test.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_picker/number_picker.dart';
+import 'package:number_picker/src/widgets/add_button.dart';
+import 'package:number_picker/src/widgets/subtract_button.dart';
 
 void main() {
   testWidgets('number picker widget', (final tester) async {
-    const int initialValue = 5;
-    const int maxValue = 3;
-    const int minValue = 5;
+    int initialValue = 5;
+    const int maxValue = 10;
+    const int minValue = 0;
+    void getValue(final int newValue) {
+      initialValue = newValue;
+    }
 
     await tester.pumpWidget(MaterialApp(
       home: Material(
         child: NumberPicker(
-          getValue: (final newValue) {
-            print('------------------------------------');
-            print('$newValue');
-          },
+          getValue: getValue,
           initialValue: initialValue,
           maxValue: maxValue,
           minValue: minValue,
@@ -23,37 +25,119 @@ void main() {
     ));
 
     final numberFinder = find.text('$initialValue');
-    final addFinder =
-        find.widgetWithIcon(InkWell, Icons.arrow_back_ios_rounded);
-    final subtractFinder =
-        find.widgetWithIcon(InkWell, Icons.arrow_forward_ios_rounded);
+    final addFinder = find.byType(AddButton);
+    final subtractFinder = find.byType(SubtractButton);
 
     expect(numberFinder, findsOneWidget);
     expect(addFinder, findsOneWidget);
     expect(subtractFinder, findsOneWidget);
+  });
+
+  testWidgets('add button Check', (final tester) async {
+    int initialValue = 8;
+    const int maxValue = 10;
+    const int minValue = 0;
+
+    await tester.pumpWidget(MaterialApp(
+      home: Material(
+        child: StatefulBuilder(
+          builder: (final context, final setState) => NumberPicker(
+            getValue: (final newValue) {
+              setState(() => initialValue = newValue);
+            },
+            initialValue: initialValue,
+            maxValue: maxValue,
+            minValue: minValue,
+          ),
+        ),
+      ),
+    ));
+
+    final addFinder =
+        find.widgetWithIcon(InkWell, Icons.arrow_back_ios_rounded);
+    expect(addFinder, findsOneWidget);
 
     await tester.tap(addFinder);
     await tester.pump();
     await tester.tap(addFinder);
     await tester.pump();
+    await tester.tap(addFinder);
+    await tester.pump();
+    await tester.tap(addFinder);
+    await tester.pump();
 
-    final enabledAddFinder = find.byWidget(
-      const Icon(
-        Icons.arrow_back_ios_rounded,
-        size: 18,
-        color: Color(0xffff6a11),
+    expect(find.text('$initialValue'), findsOneWidget);
+  });
+
+  testWidgets('subtract button check', (final tester) async {
+    int initialValue = 3;
+    const int maxValue = 10;
+    const int minValue = 0;
+
+    await tester.pumpWidget(MaterialApp(
+      home: Material(
+        child: StatefulBuilder(
+          builder: (final context, final setState) => NumberPicker(
+            getValue: (final newValue) {
+              setState(() => initialValue = newValue);
+            },
+            initialValue: initialValue,
+            maxValue: maxValue,
+            minValue: minValue,
+          ),
+        ),
       ),
-    );
+    ));
 
-    final enabledSubtractFinder = find.byWidget(
-      const Icon(
-        Icons.arrow_forward_ios_rounded,
-        size: 18,
-        color: Color(0xffff6a11),
-      ),
-    );
+    final subtractFinder =
+        find.widgetWithIcon(InkWell, Icons.arrow_forward_ios_rounded);
+    expect(subtractFinder, findsOneWidget);
 
-    // expect(enabledAddFinder, findsOneWidget);
-    // expect(enabledSubtractFinder, findsOneWidget);
+    await tester.tap(subtractFinder);
+    await tester.pump();
+    await tester.tap(subtractFinder);
+    await tester.pump();
+    await tester.tap(subtractFinder);
+    await tester.pump();
+    await tester.tap(subtractFinder);
+    await tester.pump();
+
+    expect(find.text('$initialValue'), findsOneWidget);
+  });
+
+  testWidgets('min max value check', (final tester) async {
+    int initialValue = 8;
+    const int maxValue = 0;
+    const int minValue = 10;
+    void getValue(final int newValue) {
+      initialValue = newValue;
+    }
+
+    expect(() {
+      NumberPicker(
+        getValue: getValue,
+        initialValue: initialValue,
+        maxValue: maxValue,
+        minValue: minValue,
+      );
+    }, throwsAssertionError);
+  });
+
+  testWidgets('initialValue range check', (final tester) async {
+    int initialValue = 18;
+    const int maxValue = 10;
+    const int minValue = 0;
+    void getValue(final int newValue) {
+      initialValue = newValue;
+    }
+
+    expect(() {
+      NumberPicker(
+        getValue: getValue,
+        initialValue: initialValue,
+        maxValue: maxValue,
+        minValue: minValue,
+      );
+    }, throwsAssertionError);
   });
 }
